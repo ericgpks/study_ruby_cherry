be1c58029f6f054be528de53fccc2e0c63c8a31e
ericgpks <sq.wacbc5.nyc@gmail.com>
2020-08-11 23:16:56 +0900

    add chapter5 content

--- chapter5.rb
+++ chapter5.rb
@@ -45,4 +45,36 @@ def buy_burger2(menu, drink:, potato:)
 end
 
 buy_burger2('fish', drink: true, potato: true)
-buy_burger2('fish', drink: true)
+# buy_burger2('fish', drink: true)
+
+currencies = { japan: 'yen', us: 'doller', india: 'rupee'}
+p currencies.keys
+p currencies.values
+p currencies.has_key?(:japan)
+p currencies.has_key?(:italy)
+p currencies.key?(:japan)
+p currencies.include?(:japan)
+p currencies.member?(:japan)
+
+h = { us: 'dollar', india: 'rupee' }
+h = { japan: 'yen', **h }
+p h
+h = { italy: 'euro'}.merge(h)
+p h
+array = h.to_a
+p array
+hash = array.to_h
+p hash
+
+p %s!ruby is fun!
+p %s(ruby is fun)
+
+a = nil
+p a&.upcase
+a = 'b'
+p a&.upcase
+
+limit = 10
+limit ||= 10
+p limit
+
